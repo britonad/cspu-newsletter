c5558a7a7d582fabbda165885d8afb9d65e75966
Vladyslav Kryalsov <vladyslav.krylasov@gmail.com>
2017-11-01 00:10:47 +0200

    Add a new util to check files

--- utils/common.py
+++ utils/common.py
@@ -4,3 +4,12 @@ from urllib.parse import urlparse
 def parse_url(url):
     site_url = urlparse(url)
     return '{}://{}'.format(site_url.scheme, site_url.hostname)
+
+
+def check_uploaded_files(data):
+    files = []
+    for field in data:
+        if hasattr(field, 'filename'):
+            if field.filename:
+                files.append(field)
+    return files

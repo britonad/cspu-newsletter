305d4030c0aa79cc5def3a90f0c3cd23da9b7dbb
Vladyslav Kryalsov <vladyslav.krylasov@gmail.com>
2017-12-18 19:05:30 +0200

    Delete unneeded stuff

--- utils/common.py
+++ utils/common.py
@@ -1,11 +1,3 @@
-from urllib.parse import urlparse
-
-
-def parse_url(url):
-    site_url = urlparse(url)
-    return '{}://{}'.format(site_url.scheme, site_url.hostname)
-
-
 def check_uploaded_files(data):
     files = []
     for field in data:
